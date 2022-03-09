8f1cb842b641934f85b6435c36a37a279318962e
fioresimone <simonefiore88@gmail.com>
2022-03-09 17:58:51 +0100

    f

--- src/Components/Topbar/Topbar.js
+++ src/Components/Topbar/Topbar.js
@@ -6,29 +6,24 @@ export default function Topbar() {
     <>
       <div
         className={
-          "w-screen h-8 absolute flex items-center z-50  justify-between " +
+          "w-screen h-8 absolute flex items-center z-50  justify-around " +
           classes.topbar
         }
       >
         <div className={classes.leftBlockTop}></div>
         <div className={classes.rightBlockTop}></div>
 
-        {/* <div className="w-50">
-        <Link to="/" className={classes.btn + " mx-4"}>
-          <i className="fal fa-solar-system text-white "></i>
+        <Link to="/" className="text-xs">
+          home
         </Link>
-        <Link to="/station" className={classes.btn + " mx-4"}>
-          <i className="fal fa-space-station-moon-alt text-white"></i>
+
+        <Link to="/" className="text-xs">
+          home
         </Link>
-        <Link to="/surface" className={classes.btn + " mx-4"}>
-          <i className="fal fa-hexagon text-white "></i>
+
+        <Link to="/" className="text-xs">
+          home
         </Link>
-        
-        <Link to="/spacecrafts/voyager" className={classes.btn + " mx-4"}>
-          <i className="fal fa-starship text-white "></i>
-        </Link>
-        
-      </div> */}
       </div>
       <div
         className={
